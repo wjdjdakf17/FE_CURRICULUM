1c2117c2f2be7e8fee961931047079540446d92e
kim taeyuen <wjdjdakf1007@gmail.com>
2024-03-26 20:09:32 +0900

    feat: [9] javascript 개발환경

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,109 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new ESLintPlugin(),
-  ],
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
+const dotenv = require('dotenv');
+
+module.exports = (env,argv) =>{
+  const prod = argv.mode;
+  prod === 'production' ? dotenv.config({path: './.env'}) : dotenv.config({path: './dev.env'}) 
+  return {
+    mode: process.env.API_SERVER,
+    devtool: process.env.SOURCE_MAP,
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
     },
-    hot: true,
-    port: 8080,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.s[ac]ss/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new ESLintPlugin(),
     ],
-  },
-  resolve: {
-    extensions: ['.js'],
-  },
-  devtool: 'eval-source-map',
-}
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      hot: true,
+      port: 8080,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.s[ac]ss/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js'],
+    }
+    }
+  }
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'main.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './index.html',
+//     }),
+//     new ESLintPlugin(),
+//   ],
+//   devServer: {
+//     static: {
+//       directory: path.resolve(__dirname, 'dist'),
+//     },
+//     hot: true,
+//     port: 8080,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//       {
+//         test: /\.s[ac]ss/i,
+//         use: [
+//           'style-loader',
+//           {
+//             loader: 'css-loader',
+//             options: {
+//               sourceMap: true,
+//             },
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.(js)$/,
+//         exclude: /node_modules/,
+//         use: 'babel-loader',
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['.js'],
+//   },
+//   devtool: 'eval-source-map',
+// }
