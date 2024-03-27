dc1175884dc33ad709de4dda5fbe6831f01ae8a3
kim taeyuen <wjdjdakf1007@gmail.com>
2024-03-27 10:31:26 +0900

    feat: [9] javascript 개발환경

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ const ESLintPlugin = require('eslint-webpack-plugin')
 const dotenv = require('dotenv');
 
 module.exports = (env,argv) =>{
-  const prod = argv.mode;
-  prod === 'production' ? dotenv.config({path: './.env'}) : dotenv.config({path: './dev.env'}) 
+  const mode = argv.mode;
+  mode === 'production' ? dotenv.config({path: './.env'}) : dotenv.config({path: './dev.env'}) 
   return {
-    mode: process.env.API_SERVER,
+    mode: process.env.NODE_ENV,
     devtool: process.env.SOURCE_MAP,
     entry: './src/index.js',
     output: {
@@ -57,53 +57,4 @@ module.exports = (env,argv) =>{
     }
     }
   }
-// module.exports = {
-//   mode: 'development',
-//   entry: './src/index.js',
-//   output: {
-//     filename: 'main.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: './index.html',
-//     }),
-//     new ESLintPlugin(),
-//   ],
-//   devServer: {
-//     static: {
-//       directory: path.resolve(__dirname, 'dist'),
-//     },
-//     hot: true,
-//     port: 8080,
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//       {
-//         test: /\.s[ac]ss/i,
-//         use: [
-//           'style-loader',
-//           {
-//             loader: 'css-loader',
-//             options: {
-//               sourceMap: true,
-//             },
-//           },
-//         ],
-//       },
-//       {
-//         test: /\.(js)$/,
-//         exclude: /node_modules/,
-//         use: 'babel-loader',
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: ['.js'],
-//   },
-//   devtool: 'eval-source-map',
-// }
+
